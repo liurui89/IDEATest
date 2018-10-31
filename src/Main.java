4ab11a1b1634e1b04e19b66a70a78d8049b0afe9
liurui6 <liurui6@cn.wilmar-intl.com>
2018-10-31 15:56:22 +0800

    create project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long longTime = new Date().getTime();
+
+        System.out.println("字符串类型的Long日期转换成日期:");
+        String str = "1540381294000";
+        Long dateLong = Long.valueOf(str);
+        System.out.println("longToDate："+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(dateLong)));
+
+        System.out.println("============================");
+
+        System.out.println("Date和Long互转:");
+        System.out.println("日期转换成Long："+longTime);
+    }
+}
