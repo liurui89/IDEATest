953606edb47f58187532c01381bba050212cf8e4
liurui6 <liurui6@cn.wilmar-intl.com>
2018-11-01 09:17:15 +0800

    增加注释

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,13 @@ public class Main {
         long longTime = new Date().getTime();
 
         System.out.println("字符串类型的Long日期转换成日期:");
+        System.out.println("test");
 
 
 
 
         String str = "1540381294000";
-        Long dateLong = Long.valueOf(str);
+        long dateLong = Long.parseLong(str);
         System.out.println("longToDate："+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(dateLong)));
 
         System.out.println("============================");
